ab07fd08108b4517c87b4b077a2c8bbfd1d405d0
Georgi213 <77331773+Georgi213@users.noreply.github.com>
2021-10-12 10:01:37 +0300

    Update kalkulaator1.js

--- kalkulaator1.js
+++ kalkulaator1.js
@@ -1,85 +1,116 @@
-var vastus=document.getElementById("vastus");                              
-var kogus=document.getElementById("kogus");                                
-var vastus2=document.getElementById("vastus2");                            
-var kogus2=document.getElementById("kogus2");                              
-var pilt=document.getElementById("pilt");                                  
-var age = document.getElementById("age");                                  
-var height = document.getElementById("height");                            
-var weight = document.getElementById("weight");                            
-var male = document.getElementById("m");                                   
-var female = document.getElementById("f");                                 
-var form = document.getElementById("form");                                
-const dollar=1.16;                                                         
-const kroon=10.65;                                                         
-const rub=84.20;                                                           
-const frank=1.077;                                                         
-//korrutab                                                                 
-                                                                           
-function radioChange(event){                                               
-var inputValue=kogus.value;                                                
-    if(event.target.id=="dollar"){                                         
-        vastus.innerHTML=calculate(inputValue,dollar)+ "$";                
-        pilt.src="img/dollar.png";                                         
-    }                                                                      
-   else if(event.target.id=="rub"){                                        
-        vastus.innerHTML=calculate(inputValue,rub)+ "₽";                   
-        pilt.src="img/rub.png";                                            
-    }                                                                      
-    else if(event.target.id=="kroon"){                                     
-        vastus.innerHTML=calculate(inputValue,kroon)+ "SEK";               
-        pilt.src="img/sek.png";                                            
-    }                                                                      
-    else if(event.target.id=="frank"){                                     
-        vastus.innerHTML=calculate(inputValue,frank)+ "CHF";               
-        pilt.src="img/chf.png";                                            
-    }                                                                      
-                                                                           
+                                                        
+ var vastus=document.getElementById("vastus");
+var kogus=document.getElementById("kogus");
+var vastus2=document.getElementById("vastus2");
+var kogus2=document.getElementById("kogus2");
+var pilt=document.getElementById("pilt");
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var male = document.getElementById("m");
+var female = document.getElementById("f");
+var form = document.getElementById("form");
+var resenie=document.getElementById("resenie");
+var otvet=document.getElementById("otvet");
+const dollar=1.16;
+const kroon=10.65;
+const rub=84.20;
+const frank=1.077;
+const kilo=0.0001;
+const metr=0.1;
+const deci=1;
+const santi=10;
+const milli=100;
+const mikron=10000;
+const nano=10000000;
+const ang=100000000;
+//korrutab
+
+function radioChange(event){
+var inputValue=kogus.value;
+    if(event.target.id=="dollar"){
+        vastus.innerHTML=calculate(inputValue,dollar)+ "$";
+        pilt.src="img/dollar.png";
+    }
+   else if(event.target.id=="rub"){
+        vastus.innerHTML=calculate(inputValue,rub)+ "₽";
+        pilt.src="img/rub.png";
+    }
+    else if(event.target.id=="kroon"){
+        vastus.innerHTML=calculate(inputValue,kroon)+ "SEK";
+        pilt.src="img/sek.png";
+    }
+    else if(event.target.id=="frank"){
+        vastus.innerHTML=calculate(inputValue,frank)+ "CHF";
+        pilt.src="img/chf.png";
+    }
+
+}
+function calculate(val,currency){
+    return(val*currency).toFixed(3);
+    //toFixed(3)-? 3 märki peale komat
+}
+function selectOptionChange(event){
+    var inputValue=kogus2.value;
+    if(event.target.value==="dollar"){
+        vastus2.innerHTML= calculate(inputValue,dollar)+ "$";
+        pilt.src="img/dollar.png";
+    }
+    else if(event.target.value==="rub"){
+        vastus2.innerHTML= calculate(inputValue,rub)+ "₽";
+        pilt.src="img/rub.png";
+    }
+    else if(event.target.value==="kroon"){
+        vastus2.innerHTML= calculate(inputValue,kroon)+ "SEK";
+        pilt.src="img/sek.png";
+    }
+    else if(event.target.value==="frank"){
+        vastus2.innerHTML= calculate(inputValue,frank)+ "CHF";
+        pilt.src="img/chf.png";
+    }
+}
+function inputValutaText(){
+    var inputValue=document.getElementById("kogus3").value;
+    var inputText=document.getElementById("valuutanimi").value;
+    var vastus3=document.getElementById("vastus3");
+    if(inputText=="dollar"){
+        vastus3.innerHTML=calculate(inputValue,dollar)+"$";
+
+    }
+    else if(inputText=="rub"){
+        vastus3.innerHTML=calculate(inputValue,rub)+"₽";
+
+    }
+    if(inputText=="kroon") {
+
+        vastus3.innerHTML = calculate(inputValue, kroon) + "SEK";
+    }
+    if(inputText=="frank") {
+        vastus3.innerHTML = calculate(inputValue, frank) + "CHF";
+    }
+
+}
+function perevodsm(event){
+    var inputValue=resenie.value;
+    if(event.target.value === "kilo") {
+        otvet.innerHTML = calculate(inputValue, kilo) + " km";
+    } else if(event.target.value === "metr"){
+        otvet.innerHTML=calculate(inputValue, metr) + " m";
+    } else if(event.target.value === "deci"){
+        otvet.innerHTML=calculate(inputValue, deci) + " dm";
+    } else if(event.target.value === "canti"){
+        otvet.innerHTML=calculate(inputValue, santi) + " cm";
+    } else if(event.target.value === "mili"){
+        otvet.innerHTML=calculate(inputValue, milli) + " mm";
+    } else if(event.target.value === "mikron"){
+        otvet.innerHTML=calculate(inputValue, mikron) + " mkm";
+    } else if(event.target.value === "nano"){
+        otvet.innerHTML=calculate(inputValue, nano) + " nm";
+    } else if(event.target.value === "ang"){
+        otvet.innerHTML=calculate(inputValue, ang) + " A";
+    }
+
 }                                                                          
-function calculate(val,currency){                                          
-    return(val*currency).toFixed(3);                                       
-    //toFixed(3)-? 3 märki peale komat                                     
-}                                                                          
-function selectOptionChange(event){                                        
-    var inputValue=kogus2.value;                                           
-    if(event.target.value==="dollar"){                                     
-        vastus2.innerHTML= calculate(inputValue,dollar)+ "$";              
-        pilt.src="img/dollar.png";                                         
-    }                                                                      
-    else if(event.target.value==="rub"){                                   
-        vastus2.innerHTML= calculate(inputValue,rub)+ "₽";                 
-        pilt.src="img/rub.png";                                            
-    }                                                                      
-    else if(event.target.value==="kroon"){                                 
-        vastus2.innerHTML= calculate(inputValue,kroon)+ "SEK";             
-        pilt.src="img/sek.png";                                            
-    }                                                                      
-    else if(event.target.value==="frank"){                                 
-        vastus2.innerHTML= calculate(inputValue,frank)+ "CHF";             
-        pilt.src="img/chf.png";                                            
-    }                                                                      
-}                                                                          
-function inputValutaText(){                                                
-    var inputValue=document.getElementById("kogus3").value;                
-    var inputText=document.getElementById("valuutanimi").value;            
-    var vastus3=document.getElementById("vastus3");                        
-    if(inputText=="dollar"){                                               
-        vastus3.innerHTML=calculate(inputValue,dollar)+"$";                
-                                                                           
-    }                                                                      
-    else if(inputText=="rub"){                                             
-        vastus3.innerHTML=calculate(inputValue,rub)+"₽";                   
-                                                                           
-    }                                                                      
-    if(inputText=="kroon") {                                               
-                                                                           
-        vastus3.innerHTML = calculate(inputValue, kroon) + "SEK";          
-    }                                                                      
-    if(inputText=="frank") {                                               
-        vastus3.innerHTML = calculate(inputValue, frank) + "CHF";          
-    }                                                                      
-                                                                           
-}                                                                          
-                                                                           
                                                                            
                                                                            
                                                                            
